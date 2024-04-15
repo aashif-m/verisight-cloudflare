a5b170adb580f7e3fc76b5cde9871682c8292aa0
Mohamed Aashif <maashif011@gmail.com>
2024-04-15 07:17:05 +0530

    make change password more robust

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,7 +2,7 @@ import { PrismaD1 } from "@prisma/adapter-d1";
 import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 import { jwt } from "hono/jwt";
-import { hashPassword } from "../utils/password";
+import { hashPassword, verifyPassword } from "../utils/password";
 
 type Env = {
     DB: D1Database,
@@ -18,25 +18,59 @@ app.use('/*', (c, next) => {
     return jwtMiddleware(c, next)
 })
 
-app.put("/password", async (c) => {
+app.patch("/password", async (c) => {
     const payload = c.get('jwtPayload');
-    const { password } = await c.req.json();
+    const { currentPassword, newPassword } = await c.req.json();
     const adapter = new PrismaD1(c.env.DB);
     const prisma = new PrismaClient({ adapter });
 
-    const hashedPassword = await hashPassword(password);
+    const user = await prisma.user.findUnique({
+        where: {
+            id: payload.sub,
+        },
+    });
+
+    if (!user) {
+        c.status(404);
+        return c.json({ message: "User not found" });
+    }
+
+    const currentPasswordHash = user.password;
+
+    const passwordValid = await verifyPassword(currentPasswordHash, currentPassword);
+    if (!passwordValid) {
+        c.status(401);
+        return c.json({ message: "Invalid password" });
+    }
+
+    const newHashedPassword = await hashPassword(newPassword);
 
     await prisma.user.update({
         where: {
             id: payload.sub,
         },
         data: {
-            password: hashedPassword,
+            password: newHashedPassword,
         },
     });
 
     c.status(204);
     return c.json({ message: "Password updated" });
+});
+
+app.delete("/", async (c) => {
+    const payload = c.get('jwtPayload');
+    const adapter = new PrismaD1(c.env.DB);
+    const prisma = new PrismaClient({ adapter });
+
+    await prisma.user.delete({
+        where: {
+            id: payload.sub,
+        },
+    });
+
+    c.status(204);
+    return c.json({ message: "User deleted" });
 });
 
 export default app;
