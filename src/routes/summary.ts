32e156c7181c12578765a2e6b60c654a02fee398
Mohamed Aashif <maashif011@gmail.com>
2024-05-11 13:25:58 +0530

    Change from cloudflare AI to OpenAI
    CloudflareAI context is low :(

--- src/routes/summary.ts
+++ src/routes/summary.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { jwt } from "hono/jwt";
+import OpenAI from "openai";
 
 type Env = {
     AI: any;
     JWT_SECRET: string;
+    AZURE_OPENAI_API_KEY: string;
 }
 
 type reqBody = {
@@ -13,30 +15,53 @@ type reqBody = {
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.use('/*', (c, next) => {
-    const jwtMiddleware = jwt({
-        secret: c.env.JWT_SECRET,
-    })
-    return jwtMiddleware(c, next)
-})
+// app.use('/*', (c, next) => {
+//     const jwtMiddleware = jwt({
+//         secret: c.env.JWT_SECRET,
+//     })
+//     return jwtMiddleware(c, next)
+// })
 
 app.post("/", async (c) => {
     const {headline, body} : reqBody = await c.req.json();
-    let shortenedBody = body.substring(0, 5700);
 
     const messages = [
         {
             role: "system",
-            content: "Given a news headline and body, return a summary of the news."
+            content: "Given a news headline and body, return a summary of the news. Make sure not to have any prefix."
         },
         {
             role: "user",
-            content: "Headline: " + headline + "\nBody: " + shortenedBody
+            content: "Headline: " + headline + "\nBody: " + body
         },
     ];
 
 
-    const response = await c.env.AI.run("@cf/meta/llama-3-8b-instruct", {messages});
+    // Disabled Cloudflare AI until higher context limits are announced
+    // const response = await c.env.AI.run("@cf/meta/llama-3-8b-instruct", {messages});
+
+    // Back to OpenAI :)
+    const resource = 'verisightgptapi'; //without the .openai.azure.com
+    const model = 'Verisight-gpt35-turbo-1106';
+    const apiVersion = '2024-02-15-preview';
+    // const apiKey = c.env.AZURE_OPENAI_API_KEY;
+    const apiKey = '59d96e10c65d4503943ecfb411257eba';
+  
+
+    const azure_openai = new OpenAI({
+        apiKey: apiKey,
+        baseURL: `https://gateway.ai.cloudflare.com/v1/242f6efe1a95baaf1fbdfef2f3d0654c/summary/azure-openai/${resource}/${model}`,
+        defaultQuery: { 'api-version': apiVersion },
+        defaultHeaders: { 'api-key': apiKey },
+      });
+
+    const completion = await azure_openai.chat.completions.create({
+    messages: messages,
+    model: model
+    });
+
+    const response = completion.choices[0].message.content;
+    
     c.status(201);
     return c.json(response);
 
