769c487b7260a9512286245df23a533283e5049c
Mohamed Aashif <maashif011@gmail.com>
2024-05-12 20:41:07 +0530

    move incongruence detection to openAI

--- src/routes/incongruence.ts
+++ src/routes/incongruence.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { jwt } from "hono/jwt";
+import OpenAI from "openai";
 
 type Env = {
     AI: any;
     JWT_SECRET: string;
+    AZURE_OPENAI_API_KEY: string;
 }
 
 type reqBody = {
@@ -28,23 +30,47 @@ app.post("/", async (c) => {
         {
             role: "system",
             content:
-                `Given a news headline and its corresponding body text, return one of the following values:
+                `Given a news headline and its corresponding body text, return one of the following values in json format:
 
           1. "congruent" - if the body text is congruent with the headline.
           2. "incongruent" - if the body text is incongruent with the headline.
-          
-          Remember, your response should only be the value: "congruent" or "incongruent", without any additional information or analysis.`,
+          `,
         },
         {
             role: "user",
             content: "Headline: " + headline + "\nBody: " + shortenedBody,
         }];
 
-    const response = await c.env.AI.run("@cf/meta/llama-3-8b-instruct", { messages });
-    const responseString = String(response);
-    let result = responseString.includes("incongruent") ? "incongruent" : "congruent";
+    // const response = await c.env.AI.run("@cf/meta/llama-3-8b-instruct", { messages });
+    // const responseString = String(response);
+    // let result = responseString.includes("incongruent") ? "incongruent" : "congruent";
+
+    // Back to OpenAI :)
+    const resource = 'verisightgptapi'; //without the .openai.azure.com
+    const model = 'Verisight-gpt35-turbo-1106';
+    const apiVersion = '2024-02-15-preview';
+    const apiKey = c.env.AZURE_OPENAI_API_KEY;
+
+    const azure_openai = new OpenAI({
+        apiKey: apiKey,
+        baseURL: `https://gateway.ai.cloudflare.com/v1/242f6efe1a95baaf1fbdfef2f3d0654c/summary/azure-openai/${resource}/${model}`,
+        defaultQuery: { 'api-version': apiVersion },
+        defaultHeaders: { 'api-key': apiKey },
+    });
+
+    const completion = await azure_openai.chat.completions.create({
+        messages: messages,
+        model: model,
+        response_format: { type: "json_object" }
+    });
+
+    // parse json from completion 
+    const { result } = JSON.parse(completion.choices[0].message.content);
+
     c.status(201);
-    return c.json({response: result});
+    return c.json({
+        response: result
+    });
 });
 
 export default app;
